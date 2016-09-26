57e5ce5f46cd47ea7a37d3bc7378a9662e609df2
Peter Noyes <pnoyes@gmail.com>
2016-09-25 20:12:32 -0700

    Flashing via chrome app

---
+++ assets/script/flash.go
@@ -0,0 +1,156 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gopherjs/gopherjs/js"
+	"github.com/gopherjs/jquery"
+	"strconv"
+)
+
+type Port struct {
+	*js.Object
+	Name string `js:"name"`
+}
+
+type Message struct {
+	*js.Object
+	Message string `js:"message"`
+}
+
+type Fram struct {
+	*js.Object
+	Fram []byte `js:"fram"`
+	Path string `js:"path"`
+}
+
+func flashLogic() {
+	var data []byte
+	activePort := ""
+
+	chrome := js.Global.Get("chrome")
+	if chrome != nil && chrome != js.Undefined {
+		msg := Message{Object: js.Global.Get("Object").New()}
+		msg.Message = "version"
+
+		js.Global.Get("chrome").Get("runtime").Call("sendMessage", "jnfidbnlancdlcefikapeegaccmeicmb", msg, func(reply *js.Object) {
+			go func() {
+				if reply != nil && reply != js.Undefined {
+					v := reply.Get("version").Float()
+					if v >= 1.0 {
+						jQuery("#flashButton").Show()
+					}
+				}
+			}()
+		})
+	}
+
+	jQuery("#flashBeginButton").On(jquery.CLICK, func() {
+		go func() {
+			if activePort != "" && data != nil {
+
+				fmt.Println("About to connect")
+
+				jQuery("#dropdownMenuPorts").SetProp("disabled", true)
+				jQuery("#flashBeginButton").SetProp("disabled", true)
+
+				portObj := Port{Object: js.Global.Get("Object").New()}
+				portObj.Name = "dodo_flash"
+
+				port := js.Global.Get("chrome").Get("runtime").Call("connect", "jnfidbnlancdlcefikapeegaccmeicmb", portObj)
+
+				fmt.Println(port)
+
+				dataObj := Fram{Object: js.Global.Get("Object").New()}
+				dataObj.Fram = data
+				dataObj.Path = activePort
+
+				port.Call("postMessage", dataObj)
+
+				port.Get("onMessage").Call("addListener", func(reply *js.Object) {
+					go func() {
+						if reply != nil && reply != js.Undefined {
+							progressObj := reply.Get("progress")
+							if progressObj != nil && progressObj != js.Undefined {
+								progress := progressObj.Int()
+								jQuery("#flashprogress").SetCss("width", strconv.Itoa(progress)+"%").SetAttr("aria-valuenow", progress)
+							} else {
+								successObj := reply.Get("success")
+								if successObj != nil && successObj != js.Undefined {
+									success := successObj.Bool()
+									if success {
+
+									} else {
+
+									}
+								} else {
+									// Check Error Obj
+								}
+							}
+						}
+					}()
+				})
+			}
+		}()
+	})
+
+	click := js.MakeFunc(func(this *js.Object, arguments []*js.Object) interface{} {
+		go func() {
+			activePort = jQuery(this).Text()
+			jQuery("#activePort").SetHtml(jQuery(this).Html())
+			jQuery("#flashBeginButton").SetProp("disabled", false)
+		}()
+		return nil
+	})
+
+	jQuery("#dropdownMenuPortsItems").On(jquery.CLICK, "li a", click)
+
+	jQuery("#flashButton").On(jquery.CLICK, func() {
+		go func() {
+			activePort = ""
+
+			jQuery("#flashBeginButton").SetProp("disabled", true)
+
+			setStatus("Compiling...", "bg-info")
+
+			var err error
+			data, _, err = compileCode()
+			if err != nil {
+				setStatus(err.Error(), "bg-danger")
+				return
+			}
+
+			setStatus("Flashing...", "bg-info")
+
+			jQuery("#dropdownMenuPorts").SetProp("disabled", false)
+			jQuery("#dropdownMenuPortsItems").Children("").Remove()
+			jQuery("activePort").SetHtml("Select a COM Port")
+			jQuery("#flashprogress").SetCss("width", "0%").SetAttr("aria-valuenow", 0)
+
+			msg := Message{Object: js.Global.Get("Object").New()}
+			msg.Message = "devices"
+
+			js.Global.Get("chrome").Get("runtime").Call("sendMessage", "jnfidbnlancdlcefikapeegaccmeicmb", msg, func(reply *js.Object) {
+				go func() {
+					if reply != nil && reply != js.Undefined {
+						l := reply.Length()
+						for i := 0; i < l; i++ {
+							port := reply.Index(i)
+							path := port.Get("path").String()
+							displayName := path
+
+							obj := port.Get("displayName")
+							if obj != js.Undefined {
+								//displayName = obj.String()
+							}
+
+							jQuery("#dropdownMenuPortsItems").Append("<li><a href='#'>" + displayName + "</a></li>")
+						}
+					}
+				}()
+			})
+
+			jQuery("#flashModal").Call("modal")
+
+		}()
+	})
+}
---
+++ assets/script/run.go
@@ -0,0 +1,155 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gopherjs/gopherjs/js"
+	"github.com/gopherjs/jquery"
+	"github.com/peternoyes/dodo-sim"
+	"strconv"
+	"time"
+)
+
+func runLogic(s *dodosim.SimulatorSync) {
+	jQuery("#runButton").On(jquery.CLICK, func() {
+		go func() {
+			fmt.Println("Compile Initiated...")
+
+			setStatus("Compiling...", "bg-info")
+
+			data, id, err := compileCode()
+			if err != nil {
+				setStatus(err.Error(), "bg-danger")
+				return
+			}
+
+			setStatus("Loading Simulator...", "bg-success")
+
+			fram = data
+
+			s.SwitchFram(fram)
+
+			gamelink := js.Global.Get("document").Get("location").Get("origin").String()
+			gamelink += "/?code="
+			gamelink += id
+
+			jQuery("#gamelink").SetText(gamelink)
+
+			jQuery("#simModal").Call("modal")
+
+			runSimulator(s)
+
+			setStatus("Success! Simulator Running.", "bg-success")
+		}()
+	})
+
+	jQuery("#simModal").On("hidden.bs.modal", func() {
+		go func() {
+			fmt.Println("About to stop simulator")
+			stopSimulator()
+			setStatus("Simulator Stopped.", "bg-success")
+		}()
+	})
+
+	jQuery("#resetButton").On(jquery.CLICK, func() {
+		go func() {
+			s.Cpu.Reset(s.Bus)
+		}()
+	})
+
+	jQuery("#muteButton").On(jquery.CLICK, func() {
+		go func() {
+			speaker.ToggleMute()
+			jQuery("#muteButton").ToggleClass("active")
+		}()
+	})
+
+	s.CyclesPerFrame = func(cycles uint64) {
+		jQuery("#cycles").SetText(strconv.Itoa(int(cycles)))
+	}
+
+}
+
+func runSimulator(s *dodosim.SimulatorSync) {
+	stop = false
+
+	speaker.Enable()
+
+	keyState := make(map[int]bool)
+
+	downHandler := func(event *js.Object) {
+		k := event.Get("keyCode").Int()
+		event.Call("preventDefault")
+		keyState[k] = true
+	}
+
+	upHandler := func(event *js.Object) {
+		k := event.Get("keyCode").Int()
+		event.Call("preventDefault")
+		keyState[k] = false
+	}
+
+	js.Global.Call("addEventListener", "keydown", downHandler)
+	js.Global.Call("addEventListener", "keyup", upHandler)
+
+	// Measure 2 seconds worth, to figure out delay
+	start := time.Now()
+	for i := 0; i < 40; i++ {
+		s.PumpClock("")
+	}
+	elapsed := time.Since(start)
+	fmt.Println(elapsed)
+
+	// Calculate the delay necessary to achieve 20FPS
+	delay := 1.0
+	seconds := elapsed.Seconds()
+	if seconds < 1.5 {
+		delay = ((2.0 - seconds) / 40.0) * 1000.0
+	}
+
+	fmt.Println("Delay of :", delay)
+
+	Every(time.Millisecond*time.Duration(delay), func() bool {
+		k := ""
+		if keyState[37] {
+			k += "L"
+		}
+		if keyState[38] {
+			k += "U"
+		}
+		if keyState[39] {
+			k += "R"
+		}
+		if keyState[40] {
+			k += "D"
+		}
+		if keyState[65] {
+			k += "A"
+		}
+		if keyState[66] {
+			k += "B"
+		}
+
+		s.PumpClock(k)
+
+		if stop {
+			js.Global.Call("removeEventListener", "keydown", downHandler)
+			js.Global.Call("removeEventListener", "keyup", upHandler)
+		}
+
+		return !stop
+	})
+}
+
+func stopSimulator() {
+	speaker.Disable()
+	stop = true
+}
+
+func Every(duration time.Duration, fn func() bool) {
+	time.AfterFunc(duration, func() {
+		if !fn() {
+			return
+		}
+		Every(duration, fn)
+	})
+}
