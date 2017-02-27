52bfe40f016dfe55992f9311c4c1d4c35d28fcd2
Peter Noyes <pnoyes@gmail.com>
2017-02-26 21:25:49 -0800

    Updated for 1.0.1 firmware

--- assets/script/main.go
+++ assets/script/main.go
@@ -42,7 +42,7 @@ func main() {
 	c.Set("height", 128)
 
 	fram, _ = getAsset("fram.bin")
-	firmware, _ = getAsset("firmware100.bin")
+	firmware, _ = getAsset("firmware101.bin")
 
 	wr := new(WebRenderer)
 	wr.New(ctx)
